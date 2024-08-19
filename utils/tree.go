fe0d66ee8cef224396c86ab3aba14d09ccbb6652
Inshal Khan <khanmf@rknec.edu>
2024-08-20 00:07:55 +0530

    Add entire path to node
    Signed-off-by: Inshal Khan <khanmf@rknec.edu>

--- utils/tree.go
+++ utils/tree.go
@@ -12,6 +12,7 @@ type TreeNode struct {
 	Name     string      `json:"name"`
 	Inode    uint64      `json:"inode"`
 	IsDir    bool        `json:"isDir"`
+	Path     string      `json:"path"`
 	Children []*TreeNode `json:"children,omitempty"`
 }
 
@@ -30,6 +31,7 @@ func BuildTree(path string) (*TreeNode, error) {
 	node.Name = filepath.Base(path)
 	node.IsDir = fileInfo.IsDir()
 	node.Inode = inode
+	node.Path = path
 
 	if node.IsDir {
 		entries, err := os.ReadDir(path)
@@ -38,7 +40,7 @@ func BuildTree(path string) (*TreeNode, error) {
 		}
 
 		if len(entries) == 0 {
-			return nil, nil
+			return node, nil
 		}
 
 		for _, entry := range entries {
@@ -78,9 +80,9 @@ func getFileInode(filePath string) (uint64, error) {
 func PrintTree(node *TreeNode, level int) {
 	indent := strings.Repeat("  ", level)
 	if node.IsDir {
-		fmt.Printf("%sDirectory: %s, Inode: %d\n", indent, node.Name, node.Inode)
+		fmt.Printf("%sDirectory: %s, Inode: %d, Path: %s\n", indent, node.Name, node.Inode, node.Path)
 	} else {
-		fmt.Printf("%sFile: %s, Inode: %d\n", indent, node.Name, node.Inode)
+		fmt.Printf("%sFile: %s, Inode: %d, Path: %s\n", indent, node.Name, node.Inode, node.Path)
 	}
 
 	for _, child := range node.Children {
