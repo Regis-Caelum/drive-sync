6e14a402fca1b37c0bbc8f95ddf432ae5f6a38d4
Inshal Khan <khanmf@rknec.edu>
2024-08-26 23:59:39 +0530

    Add CRUDs for DriveRecord
    Signed-off-by: Inshal Khan <khanmf@rknec.edu>

--- daemon/database/db.go
+++ daemon/database/db.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"errors"
 	"fmt"
 	pb "github.com/Regis-Caelum/drive-sync/proto/generated"
+	"google.golang.org/protobuf/proto"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"log"
@@ -31,32 +33,41 @@ func init() {
 	err = DB.AutoMigrate(
 		&pb.Node{},
 		&pb.WatchList{},
-		&pb.OAuth2Token{})
+		&pb.OAuth2Token{},
+		&pb.DriveRecord{})
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 }
 
-func ClearDatabase(db *gorm.DB) {
+func ClearDatabase() {
 	var tables []string
-	db.Raw("SELECT name FROM sqlite_master WHERE type='table'").Scan(&tables)
+	DB.Raw("SELECT name FROM sqlite_master WHERE type='table'").Scan(&tables)
 
 	for _, table := range tables {
 		if table != "sqlite_sequence" {
-			db.Exec("DROP TABLE IF EXISTS " + table)
+			if table == "o_auth2_tokens" {
+				DB.Model(&pb.OAuth2Token{}).Where("table_name = ?", "o_auth2_tokens").Updates(map[string]interface{}{
+					"root": "",
+					"host": "",
+				})
+				continue
+			}
+			DB.Exec("DROP TABLE IF EXISTS " + table)
+
 			log.Printf("Dropped table: %s", table)
 		}
 	}
-	resetSequences(db)
+	resetSequences()
 }
 
-func resetSequences(db *gorm.DB) {
+func resetSequences() {
 	var tables []string
-	db.Raw("SELECT name FROM sqlite_master WHERE type='table'").Scan(&tables)
+	DB.Raw("SELECT name FROM sqlite_master WHERE type='table'").Scan(&tables)
 
 	for _, table := range tables {
 		if table != "sqlite_sequence" {
-			db.Exec("DELETE FROM sqlite_sequence WHERE name = ?", table)
+			DB.Exec("DELETE FROM sqlite_sequence WHERE name = ?", table)
 			log.Printf("Reset sequence for table: %s", table)
 		}
 	}
@@ -91,16 +102,26 @@ func CreateNode(node *pb.Node) error {
 
 // GetNode retrieves a Node record by ID in a transaction.
 func GetNode(id int32) (*pb.Node, error) {
-	var node pb.Node
+	var node *pb.Node
 	err := DB.Transaction(func(tx *gorm.DB) error {
 		return tx.First(&node, id).Error
 	})
-	return &node, err
+	return node, err
 }
 
 // UpdateNode updates an existing Node record in a transaction.
 func UpdateNode(node *pb.Node) error {
 	return DB.Transaction(func(tx *gorm.DB) error {
+		var existingNode pb.Node
+
+		if err := tx.First(&existingNode, "id = ?", node.Id).Error; err != nil {
+			return err
+		}
+
+		if proto.Equal(&existingNode, node) {
+			return nil
+		}
+
 		return tx.Save(node).Error
 	})
 }
@@ -132,16 +153,26 @@ func CreateWatchList(watchList *pb.WatchList) error {
 
 // GetWatchList retrieves a WatchList record by ID in a transaction.
 func GetWatchList(id int32) (*pb.WatchList, error) {
-	var watchList pb.WatchList
+	var watchList *pb.WatchList
 	err := DB.Transaction(func(tx *gorm.DB) error {
 		return tx.First(&watchList, id).Error
 	})
-	return &watchList, err
+	return watchList, err
 }
 
 // UpdateWatchList updates an existing WatchList record in a transaction.
 func UpdateWatchList(watchList *pb.WatchList) error {
 	return DB.Transaction(func(tx *gorm.DB) error {
+		var existingWatchList pb.WatchList
+
+		if err := tx.First(&existingWatchList, "id = ?", watchList.Id).Error; err != nil {
+			return err
+		}
+
+		if proto.Equal(&existingWatchList, watchList) {
+			return nil
+		}
+
 		return tx.Save(watchList).Error
 	})
 }
@@ -201,4 +232,56 @@ func ListAllOAuth2Tokens() ([]*pb.OAuth2Token, error) {
 		return tx.Find(&tokens).Error
 	})
 	return tokens, err
+}
+
+// CRUD for DriveRecord
+
+// CreateDriveRecord creates a new DriveRecord record in a transaction.
+func CreateDriveRecord(record *pb.DriveRecord) error {
+	return DB.Transaction(func(tx *gorm.DB) error {
+		var existingRecord pb.DriveRecord
+
+		if err := tx.Where("local_path = ?", record.LocalPath).First(&existingRecord).Error; err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+			return err
+		}
+
+		if existingRecord.Id != 0 {
+			fmt.Println("Record already exists with local_path:", record.LocalPath)
+			return fmt.Errorf("record already exists")
+		}
+
+		return tx.Create(record).Error
+	})
+}
+
+// GetDriveRecordByLocalPath retrieves an DriveRecord record by ID in a transaction.
+func GetDriveRecordByLocalPath(path string) (*pb.DriveRecord, error) {
+	var record pb.DriveRecord
+	err := DB.Transaction(func(tx *gorm.DB) error {
+		return tx.Where("local_path = ?", path).First(&record).Error
+	})
+	return &record, err
+}
+
+// UpdateDriveRecord updates an existing DriveRecord record in a transaction.
+func UpdateDriveRecord(record *pb.DriveRecord) error {
+	return DB.Transaction(func(tx *gorm.DB) error {
+		return tx.Save(record).Error
+	})
+}
+
+// DeleteDriveRecord deletes an DriveRecord record by ID in a transaction.
+func DeleteDriveRecord(id int32) error {
+	return DB.Transaction(func(tx *gorm.DB) error {
+		return tx.Delete(&pb.DriveRecord{}, id).Error
+	})
+}
+
+// ListAllDriveRecord retrieves all DriveRecord records in a transaction.
+func ListAllDriveRecord() ([]*pb.DriveRecord, error) {
+	var records []*pb.DriveRecord
+	err := DB.Transaction(func(tx *gorm.DB) error {
+		return tx.Find(&records).Error
+	})
+	return records, err
 }
