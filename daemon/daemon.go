2c3bc27c6e992685ebb9211785b68aac13791aa0
Inshal Khan <khanmf@rknec.edu>
2024-08-29 01:35:10 +0530

    Remove unnecessary go routines
    Signed-off-by: Inshal Khan <khanmf@rknec.edu>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/Regis-Caelum/drive-sync/daemon/common"
 	"github.com/Regis-Caelum/drive-sync/daemon/database"
@@ -10,96 +11,24 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
-	"sync"
 )
 
-type WatchListMap map[string]*pb.WatchList
-type NodesMap map[string]*pb.Node
-type SharedResources struct {
-	nodesMap     NodesMap
-	watchListMap WatchListMap
-	mutex        *sync.Mutex
-}
-
 var daemonChannel chan bool
-var fileMasterChannel chan pb.FILE_ACTIONS
-
-var sharedResources *SharedResources
 var watcher *fsnotify.Watcher
 var token *pb.OAuth2Token
 
-func initializeWatchList() error {
-	sharedResources.watchListMap = make(WatchListMap)
-
-	var watchList []*pb.WatchList
-	result := database.DB.Find(&watchList)
-	if result.Error != nil {
-		return result.Error
-	}
-	sharedResources.mutex.Lock()
-	for _, item := range watchList {
-		sharedResources.watchListMap[item.AbsolutePath] = item
-	}
-	sharedResources.mutex.Unlock()
-
-	for path := range sharedResources.watchListMap {
-		if !common.PathExist(path) {
-			sharedResources.mutex.Lock()
-			delete(sharedResources.watchListMap, path)
-			sharedResources.mutex.Unlock()
-		}
-	}
-
-	fileMasterChannel <- pb.FILE_ACTIONS_DELETE_WATCHLIST
-
-	return nil
-}
-
-func initializeNodes() error {
-	sharedResources.nodesMap = make(NodesMap)
-
-	var nodes []*pb.Node
-	result := database.DB.Find(&nodes)
-	if result.Error != nil {
-		return result.Error
-	}
-
-	sharedResources.mutex.Lock()
-	for _, item := range nodes {
-		sharedResources.nodesMap[item.AbsolutePath] = item
-	}
-	sharedResources.mutex.Unlock()
-
-	for path := range sharedResources.nodesMap {
-		if !common.PathExist(path) {
-			sharedResources.mutex.Lock()
-			delete(sharedResources.nodesMap, path)
-			sharedResources.mutex.Unlock()
-		}
-	}
-
-	fileMasterChannel <- pb.FILE_ACTIONS_DELETE_NODES
-
-	return nil
-}
-
 func init() {
-	sharedResources = new(SharedResources)
-	sharedResources.mutex = new(sync.Mutex)
 	token = new(pb.OAuth2Token)
-	fileMasterChannel = make(chan pb.FILE_ACTIONS, 10)
 	daemonChannel = make(chan bool)
 
-	tx, err := database.GetTx()
-	log.Println("Transaction started")
+	err := database.DB.Transaction(func(tx *gorm.DB) error {
+		tx.First(token)
+		return nil
+	})
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	tx.First(token)
-	database.CommitTx(tx)
-	log.Println("Transaction Ended")
 
 	if token.GetValue() != "" {
 		gDriveSync()
@@ -107,9 +36,12 @@ func init() {
 		fmt.Println("cannot sync files, no drive connected")
 	}
 
-	go fileUpdateDaemon()
-
 	err = initializeNodes()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = initializeDeleteFloatingNodes()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -120,82 +52,104 @@ func init() {
 	}
 
 	err = initializeWatchList()
-	if err != nil {
+	if err != nil && !errors.Is(err, gorm.ErrDuplicatedKey) {
 		log.Fatal(err)
 	}
 
 }
 
-func (n NodesMap) addNodesMap() {
-	for _, node := range n {
-		err := database.CreateNode(node)
-		if err != nil {
-			log.Println(err)
-		}
-	}
-	fmt.Println("Uploading files to drive")
-	gDriveSyncFiles()
-}
-
-func (n NodesMap) deleteNodesMap() {
-	absolutePaths := make([]string, 0, len(n))
-	for _, node := range n {
-		absolutePaths = append(absolutePaths, node.AbsolutePath)
-	}
-	err := database.DB.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Where("absolute_path NOT IN (?)", absolutePaths).Delete(&pb.Node{}).Error; err != nil {
-			return fmt.Errorf("failed to delete WatchList records: %w", err)
-		}
-
-		if err := tx.Where("absolute_path NOT IN (?)", absolutePaths).Delete(&pb.DriveRecord{}).Error; err != nil {
-			return fmt.Errorf("failed to delete DriveRecord records: %w", err)
-		}
-		return nil
-	})
+func initializeWatchList() error {
+	watchList, err := database.ListAllWatchLists()
 	if err != nil {
-		log.Println("Error:", err)
+		return err
 	}
-	gDriveDeleteFiles()
+
+	for i := len(watchList) - 1; i >= 0; i-- {
+		w := watchList[i]
+		if !common.PathExist(w.GetAbsolutePath()) {
+			err := database.DeleteWatchList(w.GetId())
+			if err != nil {
+				return err
+			}
+			// Remove the element by slicing
+			watchList = append(watchList[:i], watchList[i+1:]...)
+		} else {
+			err := traverseDirHelper(w.GetAbsolutePath())
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
 
-func (w WatchListMap) addWatchListMap() {
-	for _, watchList := range w {
-		err := watcher.Add(watchList.AbsolutePath)
-		if err != nil {
-			log.Println("Error: ", err, watchList.AbsolutePath)
-		}
-	}
-
-	for _, watchList := range w {
-		err := database.CreateWatchList(watchList)
-		if err != nil {
-			log.Println(err)
-		}
-	}
-	fmt.Println("Creating folders in drive")
-	gDriveSyncFolders()
-}
-
-func (w WatchListMap) deleteWatchListMap() {
-	absolutePaths := make([]string, 0, len(w))
-	for _, node := range w {
-		absolutePaths = append(absolutePaths, node.AbsolutePath)
-	}
-	err := database.DB.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Where("absolute_path NOT IN (?)", absolutePaths).Delete(&pb.WatchList{}).Error; err != nil {
-			return fmt.Errorf("failed to delete WatchList records: %w", err)
-		}
-
-		if err := tx.Where("absolute_path NOT IN (?)", absolutePaths).Delete(&pb.DriveRecord{}).Error; err != nil {
-			return fmt.Errorf("failed to delete DriveRecord records: %w", err)
-		}
-		return nil
-	})
+func initializeNodes() error {
+	nodes, err := database.ListAllNodes()
 	if err != nil {
-		log.Println("Error:", err)
+		return err
 	}
 
-	gDriveDeleteFolders()
+	for i := len(nodes) - 1; i >= 0; i-- {
+		n := nodes[i]
+		if !common.PathExist(n.GetAbsolutePath()) {
+			err := database.DeleteNode(n.GetId())
+			if err != nil {
+				return err
+			}
+			// Remove the node by slicing
+			nodes = append(nodes[:i], nodes[i+1:]...)
+		}
+	}
+
+	return nil
+}
+
+func initializeDeleteFloatingNodes() error {
+	nodes, err := database.ListAllNodes()
+	if err != nil {
+		return err
+	}
+
+	watchList, err := database.ListAllWatchLists()
+	if err != nil {
+		return err
+	}
+
+	driveRecords, err := database.ListAllDriveRecord()
+	if err != nil {
+		return err
+	}
+
+	for _, n := range nodes {
+		if !common.PathExist(n.GetAbsolutePath()) {
+			err = database.DeleteNode(n.GetId())
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	for _, w := range watchList {
+		if !common.PathExist(w.GetAbsolutePath()) {
+			err = database.DeleteWatchList(w.GetId())
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	for _, d := range driveRecords {
+		if !common.PathExist(d.GetLocalPath()) {
+			gDriveDeleteFromDriveRecord(d)
+			err = database.DeleteDriveRecord(d.GetId())
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
 
 func traverseDirHelper(dirPath string) error {
@@ -209,33 +163,42 @@ func traverseDirHelper(dirPath string) error {
 		return fmt.Errorf("%s is a hidden path", dirPath)
 	}
 	if !isDir {
-		if _, exists := sharedResources.nodesMap[dirPath]; !exists {
-			// Node doesn't exist, create a new one
-			sharedResources.mutex.Lock()
-			sharedResources.nodesMap[dirPath] = &pb.Node{
-				Name:         fileInfo.Name(),
-				IsDir:        isDir,
-				FileStatus:   pb.FILE_STATUS_MODIFIED,
-				UploadStatus: pb.FILE_STATUS_NOT_UPLOADED,
-				AbsolutePath: dirPath,
-			}
-			sharedResources.mutex.Unlock()
+		// Node doesn't exist, create a new one
+		node := &pb.Node{
+			Name:         fileInfo.Name(),
+			IsDir:        isDir,
+			FileStatus:   pb.FILE_STATUS_MODIFIED,
+			UploadStatus: pb.FILE_STATUS_NOT_UPLOADED,
+			AbsolutePath: dirPath,
 		}
+		err = database.CreateNode(node)
+		if err != nil {
+			return err
+		}
+		gDriveSyncFile(node)
 	} else {
 		files, err := os.ReadDir(dirPath)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return err
 		}
-		if len(files) > 0 {
-			if _, ok := sharedResources.watchListMap[dirPath]; !ok {
-				sharedResources.mutex.Lock()
-				sharedResources.watchListMap[dirPath] = &pb.WatchList{
-					Name:         fileInfo.Name(),
-					AbsolutePath: dirPath,
-				}
-				sharedResources.mutex.Unlock()
+		if len(files) >= 0 {
+			err = watcher.Add(dirPath)
+			if err != nil {
+				log.Println("Error:", err)
+				return err
 			}
+			watchList := &pb.WatchList{
+				Name:         fileInfo.Name(),
+				AbsolutePath: dirPath,
+			}
+			err = database.CreateWatchList(watchList)
+			if err != nil {
+				log.Println("Error:", err)
+			} else {
+				gDriveSyncFolder(watchList)
+			}
+
 			for _, file := range files {
 				err = traverseDirHelper(filepath.Join(dirPath, file.Name()))
 				if err != nil {
@@ -249,78 +212,80 @@ func traverseDirHelper(dirPath string) error {
 
 func handleCreate(path string) {
 	if info, err := os.Stat(path); err == nil {
-		if _, exists := sharedResources.watchListMap[path]; !exists && info.IsDir() {
-			sharedResources.mutex.Lock()
-			sharedResources.watchListMap[path] = &pb.WatchList{
-				Name:         info.Name(),
-				AbsolutePath: path,
-			}
-			sharedResources.mutex.Unlock()
+		if info.IsDir() {
 			err = traverseDirHelper(path)
 			if err != nil {
 				log.Println("Error:", err)
 			}
-			err = watcher.Add(path)
-			if err != nil {
-				log.Println("Error:", err)
-			}
-			fileMasterChannel <- pb.FILE_ACTIONS_ADD_WATCHLIST
-			fileMasterChannel <- pb.FILE_ACTIONS_ADD_NODES
-		} else if _, exists = sharedResources.nodesMap[path]; !exists {
-			sharedResources.mutex.Lock()
-			sharedResources.nodesMap[path] = &pb.Node{
+		} else {
+			node := &pb.Node{
 				Name:         info.Name(),
 				IsDir:        info.IsDir(),
 				FileStatus:   pb.FILE_STATUS_MODIFIED,
 				UploadStatus: pb.FILE_STATUS_NOT_UPLOADED,
 				AbsolutePath: path,
 			}
-			sharedResources.mutex.Unlock()
-			fileMasterChannel <- pb.FILE_ACTIONS_ADD_NODES
+			err = database.CreateNode(node)
+			if err != nil {
+				log.Println("Error:", err)
+			}
+
+			gDriveSyncFile(node)
 		}
 	}
 }
 
 func handleRemove(path string) {
-	if _, ok := sharedResources.watchListMap[path]; ok {
-		sharedResources.mutex.Lock()
-		delete(sharedResources.watchListMap, path)
-		sharedResources.mutex.Unlock()
-		fileMasterChannel <- pb.FILE_ACTIONS_DELETE_WATCHLIST
-		log.Println("Path deleted from watchlist: ", path)
-	} else if _, ok = sharedResources.nodesMap[path]; ok {
-		sharedResources.mutex.Lock()
-		delete(sharedResources.nodesMap, path)
-		sharedResources.mutex.Unlock()
-		fileMasterChannel <- pb.FILE_ACTIONS_DELETE_NODES
-		log.Println("Path deleted from nodes: ", path)
+	if _, err := os.Stat(path); err != nil {
+		handleRename(path)
 	}
 }
 
 func handleRename(oldPath string) {
-	if _, exists := sharedResources.watchListMap[oldPath]; exists {
-		for path := range sharedResources.watchListMap {
-			if strings.HasPrefix(path, oldPath) {
-				sharedResources.mutex.Lock()
-				delete(sharedResources.watchListMap, oldPath)
-				sharedResources.mutex.Unlock()
-			}
-		}
-		for path := range sharedResources.nodesMap {
-			if strings.HasPrefix(path, oldPath+"/") {
-				sharedResources.mutex.Lock()
-				delete(sharedResources.nodesMap, path)
-				sharedResources.mutex.Unlock()
-			}
-		}
-		fileMasterChannel <- pb.FILE_ACTIONS_DELETE_WATCHLIST
-		fileMasterChannel <- pb.FILE_ACTIONS_DELETE_NODES
-	} else {
-		sharedResources.mutex.Lock()
-		delete(sharedResources.nodesMap, oldPath)
-		sharedResources.mutex.Unlock()
-		fileMasterChannel <- pb.FILE_ACTIONS_DELETE_NODES
+	nodes, err := database.GetNodesWithPrefix("absolute_path", oldPath)
+	if err != nil {
+		log.Println("Error:", err)
+		return
 	}
+
+	err = database.DeleteNodeWithPrefix("absolute_path", oldPath)
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
+
+	for _, n := range nodes {
+		gDriveDeleteFiles(n)
+	}
+
+	err = database.DeleteDriveRecordsWithPrefix("local_path", oldPath)
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
+
+	watchList, err := database.GetWatchListWithPrefix("absolute_path", oldPath)
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
+
+	err = database.DeleteWatchListWithPrefix("absolute_path", oldPath)
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
+
+	for _, w := range watchList {
+		gDriveDeleteFolders(w)
+	}
+
+	err = database.DeleteDriveRecordsWithPrefix("local_path", oldPath)
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
+
 }
 
 func handleEventDaemon(event fsnotify.Event) {
@@ -342,26 +307,6 @@ func handleEventDaemon(event fsnotify.Event) {
 	}
 }
 
-func fileUpdateDaemon() {
-	for {
-		select {
-		case updateChan := <-fileMasterChannel:
-			switch updateChan {
-			case pb.FILE_ACTIONS_ADD_NODES:
-				go sharedResources.nodesMap.addNodesMap()
-			case pb.FILE_ACTIONS_ADD_WATCHLIST:
-				go sharedResources.watchListMap.addWatchListMap()
-			case pb.FILE_ACTIONS_DELETE_NODES:
-				go sharedResources.nodesMap.deleteNodesMap()
-			case pb.FILE_ACTIONS_DELETE_WATCHLIST:
-				go sharedResources.watchListMap.deleteWatchListMap()
-			default:
-				continue
-			}
-		}
-	}
-}
-
 func daemon() {
 	defer func(watcher *fsnotify.Watcher) {
 		err := watcher.Close()
@@ -370,14 +315,6 @@ func daemon() {
 		}
 	}(watcher)
 
-	for path := range sharedResources.watchListMap {
-		err := traverseDirHelper(path)
-		if err != nil {
-			log.Println("Error: ", err)
-		}
-	}
-	fileMasterChannel <- pb.FILE_ACTIONS_ADD_WATCHLIST
-	fileMasterChannel <- pb.FILE_ACTIONS_ADD_NODES
 	daemonChannel <- true
 
 	for {
